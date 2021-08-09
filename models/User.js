1f4e568acdb4cc316580a657830e79ff0854306f
Emma Mattner <emattner1997@gmail.com>
2021-08-09 19:01:27 +0930

    finishing off user schema

--- models/User.js
+++ models/User.js
@@ -24,10 +24,28 @@ const userSchema = new Schema({
     required: true,
     minlength: 5
   },
+  age: {
+    type: String,
+  },
+  livingSpace: {
+    type: String,
+  },
+  yardSize: {
+    type: String,
+  },
+  exerciseHours: {
+    type: String,
+    enum: [0, 1, 2],
+  },
+  foodCost: {
+    type: Number,
+    enum: [1, 2, 3],
+  },
+
 });
 
 // set up pre-save middleware to create password
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
@@ -37,7 +55,7 @@ userSchema.pre('save', async function(next) {
 });
 
 // compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
