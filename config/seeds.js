773565ae8ae88174ea948317fac5127fd185f01b
Emma Mattner <emattner1997@gmail.com>
2021-08-09 19:00:10 +0930

    added in breed data

--- config/seeds.js
+++ config/seeds.js
@@ -1,7 +1,21 @@
+require('dotenv').config();
+
 const db = require('./connection');
-const { User } = require('../models');
+const { User, Breed } = require('../models');
+const breedData = require('./breeds');
+
+const OneCupOfFood = 224;
+const KgForOneCup = 6800;
+
+
+function calculateFood(weight) {
+  const ratio = KgForOneCup / OneCupOfFood;
+  return Math.round(weight * 1000 / ratio);
+}
 
 db.once('open', async () => {
+  await User.deleteMany({})
+  await Breed.deleteMany({})
 
   await User.create({
     firstName: 'Pamela',
@@ -10,12 +24,27 @@ db.once('open', async () => {
     password: 'password12345',
   });
 
-  await User.create({
-    firstName: 'Elijah',
-    lastName: 'Holt',
-    email: 'eholt@testmail.com',
-    password: 'password12345'
-  });
+
+  for (const breed of breedData) {
+    const maxWeight = breed.weight.metric.split('-')[1];
+    const minWeight = breed.weight.metric.split('-')[0];
+    const foodLevel = calculateFood(maxWeight);
+    await Breed.create({
+      breed: breed.name,
+      bredFor: breed.bred_for,
+      group: breed.breed_group,
+      minWeight,
+      maxWeight,
+      image: breed.image.url,
+      countryOfOrigin: breed.origin,
+      temperament: breed.temperament,
+      minHeight: breed.height.metric.split('-')[0],
+      maxHeight: breed.height.metric.split('-')[1],
+      lifespan: breed.lifespan,
+      foodLevel,
+    })
+
+  }
 
   console.log('users seeded');
 
