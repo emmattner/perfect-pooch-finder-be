4deaa910205a30a1ac0668a3cab264cc29b6e165
Emma Mattner <emattner1997@gmail.com>
2021-08-12 15:58:16 +0930

    writing all form data inputs

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -7,11 +7,13 @@ const typeDefs = gql`
     lastName: String
     email: String
     password: String
+    confirmPassword: String
     age: String
     livingSpace: String
     yardSize: String
     exerciseHours: String
     foodCost: String
+    dogSize: String
   }
 
   type Auth {
@@ -36,13 +38,13 @@ const typeDefs = gql`
   type Query {
     user: User
     allBreeds: [Breed]
-    findBreed(breed: String, bredFor: String, minWeight: Float, maxWeight: Float, temperament: String, minHeight: Float, maxHeight: Float, group: String, foodLevel: Float, countryOfOrigin: String, lifespan: String): [Breed]
+    findBreed(breed: String, bredFor: String, minWeight: Float, maxWeight: Float, temperament: String, minHeight: Float, maxHeight: Float, group: String, foodLevel: Float, countryOfOrigin: String, lifespan: String): Breed
   }
 
   
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    updateUser(firstName: String, lastName: String, email: String, password: String!, age: String!, livingSpace: String!, yardSize: String!, exerciseHours: String!, foodCost: String!): User
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!, confirmPassword: String!, age: String!, livingSpace: String!, yardSize: String!, exerciseHours: String!, foodCost: String!, dogSize: String!): Auth
+    updateUser(firstName: String, lastName: String, email: String, password: String!, confirmPassword: String, age: String, livingSpace: String, yardSize: String, exerciseHours: String, foodCost: String, dogSize: String): User
     login(email: String!, password: String!): Auth
   }
 `;
